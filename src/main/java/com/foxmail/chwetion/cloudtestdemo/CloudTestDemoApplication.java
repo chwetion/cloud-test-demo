5c29bb47bea032d1bdeef1764563d25583338de6
chwetion <chwetion@foxmail.com>
2020-03-16 18:30:50 +0800

    update package way from jar to war

--- src/main/java/com/foxmail/chwetion/cloudtestdemo/CloudTestDemoApplication.java
+++ src/main/java/com/foxmail/chwetion/cloudtestdemo/CloudTestDemoApplication.java
@@ -3,10 +3,17 @@ package com.foxmail.chwetion.cloudtestdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.foxmail.chwetion.cloudtestdemo.dao")
-public class CloudTestDemoApplication {
+public class CloudTestDemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CloudTestDemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CloudTestDemoApplication.class, args);
